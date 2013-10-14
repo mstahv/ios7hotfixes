22a8957c19f18a2531cf4efafe7f79cc303b7c6e
Matti Tahvonen <matti@vaadin.com>
2013-10-14 15:31:18 +0300

    Refined viewport haxie to work better on ipad

--- src/main/java/org/vaadin/addon/ios7fixes/client/Ios7HackLoader.java
+++ src/main/java/org/vaadin/addon/ios7fixes/client/Ios7HackLoader.java
@@ -1,22 +1,23 @@
 package org.vaadin.addon.ios7fixes.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.RepeatingCommand;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.MetaElement;
 import com.google.gwt.dom.client.NodeList;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.Window.Navigator;
 
 public class Ios7HackLoader implements EntryPoint {
 
+    private int bootWidth;
+    private int bootHeight;
+
     public void onModuleLoad() {
+
         /*
          * Most critical issues happen in "Home Screen Web Apps". That is when
          * apps use <meta name="apple-mobile-web-app-capable" content="yes">
@@ -28,51 +29,50 @@ public class Ios7HackLoader implements EntryPoint {
          */
         if (isiOS7HomeScreenApp()) {
 
+            if (isLandscape()) {
+                bootWidth = getWindowInnerHeight();
+                bootHeight = getWindowInnerWidth();
+            } else {
+                bootHeight = getWindowInnerHeight();
+                bootWidth = getWindowInnerWidth();
+            }
+
             /* "Somewhat" working viewport settings */
             addHeightToViewPort();
-
-            // The "viewport" size correct, set explicitly set height
-            Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-
-                @Override
-                public void execute() {
-                    fixHtmlHeightToWindowInnerHeight();
-                }
-            });
 
             // ... and set it each time size changes (most often orientation
             // change)
             Window.addResizeHandler(new ResizeHandler() {
                 @Override
                 public void onResize(ResizeEvent event) {
-                    fixHtmlHeightToWindowInnerHeight();
+                    // Defer to get correct orientation, 800 ms seems to be just
+                    // enough (tested on ipad mini)
+                    new Timer() {
+                        @Override
+                        public void run() {
+                            if (!isVirtualKeyboardOn()) {
+                                fixHtmlHeightToWindowInnerHeight();
+                            }
+                        }
+                    }.schedule(800);
                 }
             });
-
-            /*
-             * With the above the body will get scrolled to bit odd positions
-             * 
-             * Note, that this cannot be added using scroll handler because
-             * scrolling due to VKB don't always fire scroll. Thus we'll just
-             * have to run the check periodically.
-             */
-            Scheduler.get().scheduleFixedDelay(new RepeatingCommand() {
-
-                @Override
-                public boolean execute() {
-                    int scrollTop = Document.get().getBody().getScrollTop();
-                    if (scrollTop != 0 && !isKeyboardOn()) {
-                        // Most likely virtual keyboard popped up finally off,
-                        // lazily scroll back when keyboard is off
-                        Document.get().getBody().setScrollTop(0);
-                    }
-                    return true;
-                }
-            }, 300);
 
             hookCustomAlerts();
         }
 
+    }
+
+    private boolean isLandscape() {
+        switch (getOrientation()) {
+        case 0:
+        case 180:
+            return false;
+        case 90:
+        case -90:
+        default:
+            return true;
+        }
     }
 
     /**
@@ -84,6 +84,27 @@ public class Ios7HackLoader implements EntryPoint {
                 && !ua.contains("Safari");
     }
 
+    private boolean isVirtualKeyboardOn() {
+        int referenceHeight = isLandscape() ? bootWidth : bootHeight;
+        int differeceToStart = Math.abs(referenceHeight
+                - getWindowInnerHeight());
+        // Allow small changes (~ status bar & e.g. hotspot notification)
+        if (differeceToStart > 100) {
+            return true;
+        }
+        return false;
+    }
+
+    private static final native void log(String s)
+    /*-{
+        $wnd.console.log(s);
+    }-*/;
+
+    private static final native void d()
+    /*-{
+        debugger;
+    }-*/;
+
     /**
      * Modifies viewport tag to include both width=device-width AND
      * height=device-height. The latter is not generally know or used, but seems
@@ -91,32 +112,36 @@ public class Ios7HackLoader implements EntryPoint {
      * This is how it works in Safari, Android, mobile IE and in previous
      * version of iOS home screen web apps.
      * <p>
-     * Without this viewport on home screen web shrinks when e.g. virtual
-     * keyboard pops on, causing all kinds of weird stuff. Setting
-     * "device-width" & "device-height" somewhat fixes the issue, but size is
-     * still bit wrong in vertical orientation, more in horizontal mode. Note
-     * that setting the "right pixel value" don't work either, I have tried
-     * that. Thus in addition to this we also define the actually correct size
-     * from window.innerHeigt to document element, which again causes some
-     * scroll issues when keyboard gets on and off, but we'll fix them too with
-     * timers :-).
+     * Instead of "device-height" we use pixel height reported by
+     * window.innerHeight. On orientation changes that must be updated.
      */
     private void addHeightToViewPort() {
+        MetaElement item = getViewportTag();
+        if (item != null) {
+            String attribute = item.getContent();
+            if (!attribute.contains("width")) {
+                attribute += ",width=device-width";
+            }
+            int viewPortHeight = getWindowInnerHeight();
+            if (!attribute.contains("height")) {
+                attribute += ",height=" + viewPortHeight;
+            } else {
+                attribute = updateViewPortHeight(attribute, viewPortHeight);
+            }
+            item.setContent(attribute);
+            ;
+        }
+    }
+
+    private MetaElement getViewportTag() {
         NodeList<Element> metas = Document.get().getElementsByTagName("meta");
         for (int i = 0; i < metas.getLength(); i++) {
             MetaElement item = metas.getItem(i).cast();
             if ("viewport".equals(item.getAttribute("name"))) {
-                String attribute = item.getAttribute("content");
-                if (!attribute.contains("width")) {
-                    attribute += ",width=device-width";
-                }
-                if (!attribute.contains("height")) {
-                    attribute += ",height=device-height";
-                }
-                item.setAttribute("content", attribute);
+                return item;
             }
         }
-
+        return null;
     }
 
     private native final void hookCustomAlerts()
@@ -134,23 +159,31 @@ public class Ios7HackLoader implements EntryPoint {
         console.log("alert,confirm and prompt now 'polyfilled'");
     }-*/;
 
-    private boolean isKeyboardOn() {
-        return Math.abs(Document.get().getBody().getOffsetHeight()
-                - getWindowInnerHeight()) > 45;
+    private void fixHtmlHeightToWindowInnerHeight() {
+        final MetaElement tag = getViewportTag();
+        int viewPortHeight = getWindowInnerHeight();
+        String c = updateViewPortHeight(tag.getContent(), viewPortHeight);
+        tag.setContent(c);
     }
 
-    private void fixHtmlHeightToWindowInnerHeight() {
-        /*
-         * window.innerHeight/Width seems to be the only thing that one can
-         * count in iOS7. We'll fix that to the problematic height
-         */
-        Document.get().getDocumentElement().getStyle()
-                .setHeight(getWindowInnerHeight(), Unit.PX);
-    }
+    private static final native String updateViewPortHeight(String s, int h)
+    /*-{
+        return s.replace(/,height=\w+/,",height=" + h);
+    }-*/;;
+
+    private static final native int getOrientation()
+    /*-{
+        return $wnd.orientation;
+    }-*/;
 
     private static native int getWindowInnerHeight()
     /*-{
         return $wnd.innerHeight;
+    }-*/;
+
+    private static native int getWindowInnerWidth()
+    /*-{
+        return $wnd.innerWidth;
     }-*/;
 
 }
